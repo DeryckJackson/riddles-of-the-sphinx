403c799626311d4a8f89ad74b268f0a93c5b1f38
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-07-13 11:19:48 -0700

    rework logic to ask a random question from an array
    co-authored-by: Frederick Ernest <frederick.ernest@gmail.com>

--- riddles.cs
+++ riddles.cs
@@ -4,16 +4,19 @@ class Program
 {
   static void Main()
   {
-    string[,] qAndA = new string[,] {{"What color is an orange?", "orange"}};
+    Random rnd = new Random();
+
+    string[,] qAndA = new string[,] {{"What color is an orange?", "orange"}, {"How many feet in a yard?", "3"}};
+    int randomIndex = rnd.Next(0, qAndA.Length - 1);
 
 
     Console.WriteLine("Welcome to the riddle of the sphinx!");
     Console.WriteLine("Please enter an answer to the provided question below.");
-    Console.WriteLine(qAndA[0, 0]);
+    Console.WriteLine(qAndA[randomIndex, 0]);
 
     string answer = Console.ReadLine();
 
-    if (answer.ToLower() == qAndA[0,1])
+    if (answer.ToLower() == qAndA[randomIndex, 1])
     {
       Console.WriteLine("Correct");
       // Console.WriteLine(questions[1]);
